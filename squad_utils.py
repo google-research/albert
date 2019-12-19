52d492842e4e5eab36b412068eed3b2a7d7142b7
freefuiiismyname <rongfuiii@163.com>
2019-12-19 18:23:05 +0800

    google_type_annotations not found error
    google_type_annotations cannot be found, and it is not being used.
    
    del line 20 in squad_utils.py
    from __future__ import google_type_annotations

--- squad_utils.py
+++ squad_utils.py
@@ -17,8 +17,8 @@
 
 from __future__ import absolute_import
 from __future__ import division
-from __future__ import google_type_annotations
 from __future__ import print_function
+
 import collections
 import json
 import math
