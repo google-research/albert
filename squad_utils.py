eda4dc230dedffb322ba7f81fdcef3f4ffef16f3
Sebastian Goodman <seabass@google.com>
2019-12-20 13:36:06 -0800

    Internal change
    PiperOrigin-RevId: 286632227

--- squad_utils.py
+++ squad_utils.py
@@ -17,7 +17,7 @@
 
 from __future__ import absolute_import
 from __future__ import division
-
+# from __future__ import google_type_annotations
 from __future__ import print_function
 import collections
 import json
